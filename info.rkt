aedbdf9e0a5f3e62a05269d65ccf23db33528139
Matthew Flatt <mflatt@racket-lang.org>
2015-11-07 07:02:58 -0700

    tests moved from the "drracket-test" package

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection "pcps-test")
+
+(define test-omit-paths '("balle-grav-frot.ss"))
+
+(define test-responsibles '((all mflatt)))
+
+(define deps '("base"
+               "gui-lib"
+               "scheme-lib"
+               "htdp-lib"))
